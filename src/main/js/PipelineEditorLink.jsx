bf9e84e6c274b2a31b13717f12ac99c7567596bd
kzantow <kzantow@gmail.com>
2017-01-31 22:57:44 -0500

    renames to preview

--- src/main/js/PipelineEditorLink.jsx
+++ src/main/js/PipelineEditorLink.jsx
@@ -4,13 +4,13 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 import Extensions from '@jenkins-cd/js-extensions';
 
-function PipelineEditorButton() {
+function PipelineEditorLink() {
     return (
-        <Link className="pipeline-editor" to="/pipelines/pipeline-editor-demo">
+        <Link className="pipeline-editor" to="/pipelines/pipeline-editor-preview">
             <Extensions.Renderer extensionPoint="pipeline.editor.css"/>
             Pipeline Editor
         </Link>
     );
 }
 
-export default PipelineEditorButton;
+export default PipelineEditorLink;
