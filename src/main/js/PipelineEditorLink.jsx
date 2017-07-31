eb06da7500359ffd586635c52fe189b1d637aade
Nicolae Pascu <contact@nicupascu.ro>
2017-07-31 15:37:18 +1000

    add title text to the edit icon

--- src/main/js/PipelineEditorLink.jsx
+++ src/main/js/PipelineEditorLink.jsx
@@ -41,7 +41,7 @@ class PipelineEditorLink extends React.Component {
         const baseUrl = `/organizations/${pipeline.organization}/pipeline-editor/${encodeURIComponent(pipelinePath.join('/'))}/${branch}/`;
 
         return (
-            <Link className="pipeline-editor-link" to={baseUrl}>
+            <Link className="pipeline-editor-link" to={baseUrl} title="Edit">
                 <Icon icon="mode_edit" size={24} />
             </Link>
         );
